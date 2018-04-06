9f71d7a16a16bb97d7bcec03fb0c6e3c839a5f1c
Michiel van Oudheusden <mvanoudheusden@xpirit.com>
2018-04-06 14:58:25 +0200

    Added other CRUD

---
+++ AkkaES.Business/Customers/RemoveCustomerCommand.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using AkkaES.Core.EventSourcing;
+
+namespace AkkaES.Business.Customers
+{
+    public class RemoveCustomerCommand : CustomerBaseCommand
+    {
+        public RemoveCustomerCommand(Guid id) : base(id)
+        {
+        
+        }
+
+    }
+}
---
+++ AkkaES.Business/Customers/UpdateCustomerCommand.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using AkkaES.Core.EventSourcing;
+
+namespace AkkaES.Business.Customers
+{
+    public class UpdateCustomerCommand : CustomerBaseCommand
+    {
+        public UpdateCustomerCommand(Guid id, string name) : base(id)
+        {
+            Name = name;
+        }
+
+        public string Name { get; }
+    }
+}
